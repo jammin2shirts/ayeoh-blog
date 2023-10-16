7c0ef774d5eedab92bf805e22718f6cd45805bda
jammin2shirts <teuchertb@gmail.com>
2023-10-16 16:04:10 -0400

    Made blog

---
+++ posts/first-blog-post.md
@@ -0,0 +1,12 @@
+---
+title: This is my first blog post!
+published_at: 2022-11-04T15:00:00.000Z
+snippet: This is an excerpt of my first blog post.
+---
+
+Hello, world!
+
+### here
+
+# wow
+_underline_
---
+++ utils/posts.ts
@@ -0,0 +1,42 @@
+import { createExtractor,Format,Parser } from "$std/front_matter/mod.ts";
+import { join } from "$std/path/mod.ts";
+import { parse } from "$std/yaml/parse.ts";
+
+const extract = createExtractor({
+  [Format.YAML]: parse as Parser,
+})
+
+export interface Post {
+    slug: string;
+    title: string;
+    published_at: Date;
+    content: string;
+    snippet: string;
+}
+
+export async function getPosts(): Promise<Post[]> {
+    const files = Deno.readDir("./posts");
+    const promises = [];
+    for await (const file of files) {
+      const slug = file.name.replace(".md", "");
+      promises.push(getPost(slug));
+    }
+    const posts = await Promise.all(promises) as Post[];
+    posts.sort((a, b) => b.published_at.getTime() - a.published_at.getTime());
+    return posts;
+  }
+  
+  // Importing two new std lib functions to help with parsing front matter and joining file paths.
+  
+  export async function getPost(slug: string): Promise<Post | null> {
+    const text = await Deno.readTextFile(join("./posts", `${slug}.md`));
+    const { attrs, body } = extract<Post>(text);
+    console.log(extract(text));
+    return {
+      slug,
+      title: attrs.title,
+      published_at: new Date(attrs.published_at),
+      content: body,
+      snippet: attrs.snippet,
+    };
+  }
